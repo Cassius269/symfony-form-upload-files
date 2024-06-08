45148c451cee2440d698e497b0b69877578ce1ee
cassius269 <fahamygaston@gmail.com>
2024-06-08 23:59:12 +0200

    création de formulaire non bindé, et gestion de l'upload de fichier

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(Request $request): Response
+    {
+
+        $form = $this->createFormBuilder()
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom:'
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom:'
+            ])
+            ->add('cv', FileType::class, [
+                'attr' => [
+                    'accept' => 'image/*',
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Envoyer'
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Récuperer le fichier
+            $file = $form["cv"]->getData();
+
+            $extension = $file->guessExtension();
+            $extensionsList = ["png", "jpg", "jpeg"];
+
+            // Vérifier si le fichier a la bonne extension, si oui déplacer dans le dossier "/public/images"
+            if (in_array($extension, $extensionsList, true)) {
+                $file->move('images', 'NouveauNomDuFichier.jpeg');
+            }
+        }
+
+        return $this->render('home/index.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
